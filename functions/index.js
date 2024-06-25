dccef16b75cb147389b06c47b90777bd98ceef72
itseclectus <itseclectus@gmail.com>
2024-06-25 09:07:42 +0330

    Update DNS
    Changed from CF to NextDNS

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://dns.nextdns.io/67e782'
+const dohjson = 'https://dns.nextdns.io/67e782'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
